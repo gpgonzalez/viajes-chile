431232d6c3419d835cee82c6a9e7495d5c550630
Gabriel Pérez González <g.andres.gonzalez@hotmail.com>
2021-11-21 13:00:23 -0300

    Re haciendo NAV

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $("a").click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var numeral= this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(numeral).offset().top   
+            }, 800, function() {
+               window.location.hash = numeral; 
+            });
+        }
+    });
+    
+});
